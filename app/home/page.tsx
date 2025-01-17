2947190d2cbba14ac825315b363865aaa6f11b86
zvvlj0219 <s00302448@gmail.com>
2025-01-17 14:53:12 +0900

    create .env.production

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,7 +3,8 @@ import { cookies, headers } from "next/headers";
 async function testFetch(): Promise<{ message: string }> {
   const origin = headers().get('host');
 
-  const response = await fetch('http://localhost:3000/api', { 
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+  const response = await fetch(`${baseUrl}/api`, {
     cache: 'no-store',
     headers: {
       accept: 'application/json',
